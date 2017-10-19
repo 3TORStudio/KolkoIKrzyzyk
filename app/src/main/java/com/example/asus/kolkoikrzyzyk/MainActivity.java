788fe271adb2413cb75cce50a98c78ac463b03e4
3TORStudio <marcinhesse@gmail.com>
2017-10-19 13:21:56 +0200

    checker works

--- app/src/main/java/com/example/asus/kolkoikrzyzyk/MainActivity.java
+++ app/src/main/java/com/example/asus/kolkoikrzyzyk/MainActivity.java
@@ -44,14 +44,18 @@ public class MainActivity extends AppCompatActivity {
             button.setEnabled(false);
         }
         setCounter("add");
-        checkTheState();
+        if(counter > 5){
+            checkTheState();
+        }
+
     }
 
     public void onClickNewGame(View view){
+        TextView text = (TextView) findViewById(R.id.textView);
+        text.setText("new");
         for(int idOfBtn : listOfClickedBtnId){
             Button button = (Button) findViewById(idOfBtn);
             button.setText(" ");
-            //button.setBackground(R.drawable.class);
             button.setEnabled(true);
         }
         setCounter("reset");
@@ -72,16 +76,37 @@ public class MainActivity extends AppCompatActivity {
 
     public void checkTheState (){
         Button btn1 = (Button) findViewById(R.id.btn11);
+        String s11 = btn1.getText().toString();
         Button btn2 = (Button) findViewById(R.id.btn12);
+        String s12 = btn2.getText().toString();
         Button btn3 = (Button) findViewById(R.id.btn13);
+        String s13 = btn3.getText().toString();
         Button btn4 = (Button) findViewById(R.id.btn21);
+        String s21 = btn4.getText().toString();
         Button btn5 = (Button) findViewById(R.id.btn22);
+        String s22 = btn5.getText().toString();
         Button btn6 = (Button) findViewById(R.id.btn23);
+        String s23 = btn6.getText().toString();
         Button btn7 = (Button) findViewById(R.id.btn31);
+        String s31 = btn7.getText().toString();
         Button btn8 = (Button) findViewById(R.id.btn32);
+        String s32 = btn8.getText().toString();
         Button btn9 = (Button) findViewById(R.id.btn33);
+        String s33 = btn9.getText().toString();
+
         TextView text = (TextView) findViewById(R.id.textView);
-        if(btn1.getText().equals(btn2.getText()) && btn2.getText().equals(btn3.getText())){
+
+        if(s11.equals(s12) && s12.equals(s13) && s11 != " " ||
+                s21.equals(s22) && s22.equals(s23) && s21 != " " ||
+                s31.equals(s32) && s32.equals(s33) && s31 != " " ||
+
+                s11.equals(s21) && s21.equals(s31) && s11 != " " ||
+                s12.equals(s22) && s22.equals(s32) && s12 != " " ||
+                s13.equals(s23) && s23.equals(s33) && s13 != " " ||
+
+                s11.equals(s22) && s22.equals(s33) && s11 != " " ||
+                s13.equals(s22) && s22.equals(s31) && s13 != " "
+                ){
             text.setText("1");
         }
     }
